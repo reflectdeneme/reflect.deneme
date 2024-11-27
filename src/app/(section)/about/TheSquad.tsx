57311cfe9e0b8690b312dadb505c4a61f72f91d4
Ceyda <ceydaonemli2@gmail.com>
2024-11-27 20:55:30 +0300

    test

--- src/app/(section)/about/TheSquad.tsx
+++ src/app/(section)/about/TheSquad.tsx
@@ -9,7 +9,7 @@ const TheSquad = async () => {
 
   return (
     <>
-      <div>burakkkkkkk babaaass</div>
+      <div>actions works</div>
       {slides.items?.map((items) => (
         <div key={items.userName}>{items.userName}</div>
       ))}
