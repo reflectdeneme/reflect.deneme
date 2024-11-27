137535c187f852d8fdf774ef1352ae1b577b2420
burakdincer@gui.com.tr <burakdinceeer@gmail.com>
2024-11-27 17:05:27 +0300

    test

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 /* eslint-disable */
-const { onRequest } = require('firebase-functions/v2/https');
-const { logger } = require('firebase-functions/v2');
-const next = require('next');
-const parse = require('url').parse;
+const { onRequest } = require("firebase-functions/v2/https");
+const { logger } = require("firebase-functions/v2");
+const next = require("next");
+const parse = require("url").parse;
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 
 const app = next({
   dev: false,
-  conf: { distDir: 'build' },
+  conf: { distDir: "build" },
 });
 const nextjsHandle = app.getRequestHandler();
 
-exports.ssrreflect = onRequest({ memory: '1GiB' }, (req, res) => {
+exports.ssrreflect = onRequest({ memory: "1GiB" }, (req, res) => {
   return app.prepare().then(() => {
     const parsedUrl = parse(req.url, true);
 
