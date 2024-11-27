72cd8f0e616add64f3103bc01c7be7ec198e3455
burakdincer@gui.com.tr <burakdinceeer@gmail.com>
2024-11-27 16:24:06 +0300

    test

---
+++ server.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+const { onRequest } = require('firebase-functions/v2/https');
+const { logger } = require('firebase-functions/v2');
+const next = require('next');
+const parse = require('url').parse;
+
+const dev = process.env.NODE_ENV !== 'production';
+
+const app = next({
+  dev: false,
+  conf: { distDir: 'build' },
+});
+const nextjsHandle = app.getRequestHandler();
+
+exports.ssrreflect = onRequest({ memory: '4GiB' }, (req, res) => {
+  return app.prepare().then(() => {
+    const parsedUrl = parse(req.url, true);
+
+    return nextjsHandle(req, res, parsedUrl);
+  });
+});
