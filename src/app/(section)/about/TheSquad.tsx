93b38922b91be0afded13b28715a251e425270ff
Berk Kasapoglu <berk@gui.com.tr>
2024-11-27 23:09:06 +0300

    test

--- src/app/(section)/about/TheSquad.tsx
+++ src/app/(section)/about/TheSquad.tsx
@@ -9,7 +9,7 @@ const TheSquad = async () => {
 
   return (
     <>
-      <div>kürt burak</div>
+      <div>Elmadağlı Ceyda</div>
       {slides.items?.map((items) => (
         <div key={items.userName}>{items.userName}</div>
       ))}
