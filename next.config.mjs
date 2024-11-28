06a03a80214808f3705887d5f97f00237fca2335
burakdincer@gui.com.tr <burakdinceeer@gmail.com>
2024-11-28 12:27:42 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "build",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+    return config;
+  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
