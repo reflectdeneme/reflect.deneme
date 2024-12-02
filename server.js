c0bce4d9bbcf04869a7a5fb47e42c265296b861a
burakdincer@gui.com.tr <burakdinceeer@gmail.com>
2024-12-02 22:36:08 +0300

    test

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const { onRequest } = require("firebase-functions/v2/https");
 const next = require("next");
 const parse = require("url").parse;
 
-const appMain = next({
-  dev: false, // Development modu kapalı
-  conf: { distDir: "build" },
-});
+// const appMain = next({
+//   dev: false, // Development modu kapalı
+//   conf: { distDir: "build" },
+// });
 
 const appRelease = next({
   dev: false, // Production modu
   conf: { distDir: "build" },
 });
 
-const nextjsHandleMain = appMain.getRequestHandler();
+// const nextjsHandleMain = appMain.getRequestHandler();
 const nextjsHandleRelease = appRelease.getRequestHandler();
 
-exports.ssrMain = onRequest({ memory: "1GiB" }, async (req, res) => {
-  await appMain.prepare();
-  const parsedUrl = parse(req.url, true);
-  nextjsHandleMain(req, res, parsedUrl);
-});
+// exports.ssrMain = onRequest({ memory: "1GiB" }, async (req, res) => {
+//   await appMain.prepare();
+//   const parsedUrl = parse(req.url, true);
+//   nextjsHandleMain(req, res, parsedUrl);
+// });
 
 exports.ssrreflect = onRequest({ memory: "1GiB" }, async (req, res) => {
   await appRelease.prepare();
--- src/app/(section)/about/TheSquad.tsx
+++ src/app/(section)/about/TheSquad.tsx
@@ -9,7 +9,7 @@ const TheSquad = async () => {
 
   return (
     <>
-      <div>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div>
+      <div>ccccccccccccccccccccccccccccccc</div>
       {slides.items?.map((items) => (
         <div key={items.userName}>{items.userName}</div>
       ))}
