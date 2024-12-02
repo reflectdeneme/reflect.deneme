79f62f5dc869cb2d7e7a6c5ecefee68472bc9be2
burakdincer@gui.com.tr <burakdinceeer@gmail.com>
2024-12-02 14:44:27 +0300

    test

--- src/app/(section)/about/TheSquad.tsx
+++ src/app/(section)/about/TheSquad.tsx
@@ -9,7 +9,7 @@ const TheSquad = async () => {
 
   return (
     <>
-      <div>hahaha</div>
+      <div>fddf</div>
       {slides.items?.map((items) => (
         <div key={items.userName}>{items.userName}</div>
       ))}
